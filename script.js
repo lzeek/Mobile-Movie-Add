7577a045c0920527c1c5a0fa0e084abc2ebc1a68
lzeek <86994603+lzeek@users.noreply.github.com>
2023-08-28 22:59:23 +0200

    app complete

---
+++ script.js
@@ -0,0 +1,29 @@
+// Add JavaScript code here
+
+let playButton = document.getElementById("play-btn");
+let video = document.getElementById("vid");
+
+playButton.addEventListener("click", () => {
+  if (video.paused || video.ended) {
+    video.play();
+    playButton.setAttribute("data-state", "hidden");
+  } else {
+    video.pause();
+    playButton.setAttribute("data-state", "visible");
+  }
+});
+
+// Add JavaScript code here
+
+// let playButton = document.getElementById("play-btn");
+// let video = document.getElementById("vid");
+
+// playButton.addEventListener("click", () => {
+//   if (video.paused || video.ended) {
+//     video.play();
+//     playButton.setAttribute("data-state", "hidden");
+//   } else {
+//     video.pause();
+//     playButton.setAttribute("data-state", "visible");
+//   }
+// });
