f8989f93ea6e8d005e3e3a662acdec6fe7ece623
lzeek <86994603+lzeek@users.noreply.github.com>
2023-08-29 23:27:26 +0200

    Update Script.js

--- script.js
+++ script.js
@@ -12,18 +12,3 @@ playButton.addEventListener("click", () => {
     playButton.setAttribute("data-state", "visible");
   }
 });
-
-// Add JavaScript code here
-
-// let playButton = document.getElementById("play-btn");
-// let video = document.getElementById("vid");
-
-// playButton.addEventListener("click", () => {
-//   if (video.paused || video.ended) {
-//     video.play();
-//     playButton.setAttribute("data-state", "hidden");
-//   } else {
-//     video.pause();
-//     playButton.setAttribute("data-state", "visible");
-//   }
-// });
